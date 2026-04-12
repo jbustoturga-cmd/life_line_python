63009bbafd6a711170f837363b2450d9b54e3c76
leidyesteflaram77 <leidyesteflaram77@gmail.com>
2026-04-12 00:44:35 -0500

    forms.py de usuarios

--- lifeline_proyecto/usuarios/forms.py
+++ lifeline_proyecto/usuarios/forms.py
@@ -8,7 +8,7 @@ from django.core.exceptions import ValidationError
 from django.utils import timezone
 from .models import Usuario
 
-
+#forms.py
 # ─────────────────────────────────────────────────────────────────────────────
 # Función helper reutilizable
 # ─────────────────────────────────────────────────────────────────────────────
