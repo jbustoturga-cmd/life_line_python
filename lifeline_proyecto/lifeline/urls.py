d1ef4a3e2c852680c22659d091b84e7962a53c0c
jbustoturga-cmd <jbustoturga@gmail.com>
2026-04-11 23:55:23 -0500

    urls.py de life_line

--- lifeline_proyecto/lifeline/urls.py
+++ lifeline_proyecto/lifeline/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+#urls.py
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('usuarios.urls')),
