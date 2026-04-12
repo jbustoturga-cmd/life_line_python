793e42ec9425907279739dcac3681f25bfec96c3
jbustoturga-cmd <jbustoturga@gmail.com>
2026-04-11 23:45:07 -0500

    models.py alarmas

--- lifeline_proyecto/alarma/models.py
+++ lifeline_proyecto/alarma/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.utils import timezone
-
+#models.py
 
 class Alarma(models.Model):
 
