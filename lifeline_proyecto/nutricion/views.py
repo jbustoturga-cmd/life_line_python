5827c40f6d46683bebb5f3f94c9db62889a1538b
leidyesteflaram77 <leidyesteflaram77@gmail.com>
2026-04-12 00:36:39 -0500

    views.py de nutricion

--- lifeline_proyecto/nutricion/views.py
+++ lifeline_proyecto/nutricion/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import PlanNutricional, SugerenciaRapida, RegistroNutricional
-
+#views.py
 # Sugerencias predefinidas (no requieren base de datos)
 SUGERENCIAS_PREDEFINIDAS = {
     'nauseas': {
