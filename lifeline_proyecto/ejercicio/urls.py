4786e5c651f86e9ef8501291454ea92eb3668648
jbustoturga-cmd <jbustoturga@gmail.com>
2026-04-11 23:53:20 -0500

    urls.py de ejercicio

--- lifeline_proyecto/ejercicio/urls.py
+++ lifeline_proyecto/ejercicio/urls.py
@@ -5,4 +5,4 @@ from . import views
 urlpatterns = [
     path('',           views.vista_ejercicio,        name='ejercicio'),
     path('completar/', views.vista_completar_sesion,  name='completar_sesion'),
-]
+] #urls.py
