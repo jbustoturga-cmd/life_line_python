05c2129b98e6310aced53c8e5bf50bb0612d3fd7
leidyesteflaram77 <leidyesteflaram77@gmail.com>
2026-04-12 00:44:11 -0500

    admin.py de usuarios

--- lifeline_proyecto/usuarios/admin.py
+++ lifeline_proyecto/usuarios/admin.py
@@ -4,7 +4,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
-
+#admin.py
 
 @admin.register(Usuario)
 class UsuarioAdmin(UserAdmin):
