69468cd556a044382657ef3a1a247d0be49e6976
leidyesteflaram77 <leidyesteflaram77@gmail.com>
2026-04-12 00:36:14 -0500

    urls.py de nutricion

--- lifeline_proyecto/nutricion/urls.py
+++ lifeline_proyecto/nutricion/urls.py
@@ -5,4 +5,4 @@ from . import views
 urlpatterns = [
     path('',          views.vista_nutricion,         name='nutricion'),
     path('registrar/',views.vista_registrar_sintoma,  name='registrar_sintoma'),
-]
+]#urls.py
