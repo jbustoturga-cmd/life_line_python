1071e53de83a19ae0d079c82a084229d62dd692c
jbustoturga-cmd <jbustoturga@gmail.com>
2026-04-11 23:56:23 -0500

    forms.py de alarma

--- lifeline_proyecto/alarma/forms.py
+++ lifeline_proyecto/alarma/forms.py
@@ -7,7 +7,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from .models import Alarma, HistorialDosis
 
-
+#forms.py
 class FormularioAlarma(forms.ModelForm):
 
     class Meta:
