36e2085234278a6e013af6e6777ed0d6d70fb7bb
jbustoturga-cmd <jbustoturga@gmail.com>
2026-04-11 23:53:41 -0500

    views.py de ejercicio

--- lifeline_proyecto/ejercicio/views.py
+++ lifeline_proyecto/ejercicio/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import SesionEjercicio
-
+#views.py
 # Rutinas predefinidas por nivel de energía
 RUTINAS = {
     'alta': {
