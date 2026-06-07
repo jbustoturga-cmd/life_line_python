c3a0e160a2c4f21d131467476578103471809090
jbustoturga-cmd <jbustoturga@gmail.com>
2026-06-07 15:11:08 -0500

    Fix static files railway

--- lifeline_proyecto/lifeline/settings.py
+++ lifeline_proyecto/lifeline/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 # ============================================================
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ============================================================
 # ARCHIVOS MEDIA — imágenes subidas por usuarios (Leidy y Jhoan)
 # ============================================================
